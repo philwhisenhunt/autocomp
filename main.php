3e2e359ee8de37c3e213e6c9fb73cd30c78c10b8
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-08-24 22:19:53 -0400

    Tried this as well but getting undefined index for bo

--- main.php
+++ main.php
@@ -15,22 +15,27 @@ fwrite($myfile, $piece);
 
 $pieceEncoded = json_encode($piece);
 
+var_dump($piece);
+
+if( strpos(file_get_contents("list.txt"),$_GET['bo']) !== false) {
+    echo "hello";
+}
 //echo $piece;
 
-$needle = '/go(\w+)/';
-echo $needle;
+// $needle = '/go(\w+)/';
+// echo $needle;
 //$needle = '/' . $piece . '(\w+)/';
-$needle = "/$piece(\w+)/";
+// $needle = "/$piece(\w+)/";
 // echo $needle;
 // $haystack = ['arg', 'goo', 'go', 'goo'];
-$haystack = ['bob', 'larry', 'sam', 'sammy'];
+//$haystack = ['bob', 'larry', 'sam', 'sammy'];
 
-$result = preg_grep ($needle, $haystack);
+// $result = preg_grep ($needle, $haystack);
 // print_r($result);
-foreach($result as $key=>$value){
-    $value = $value . "\n";
-    echo $value;
-}
+// foreach($result as $key=>$value){
+//     $value = $value . "\n";
+//     echo $value;
+// }
 // print_r($result);
 // print_r($pieceEncoded);
 // echo $pieceEncoded;
